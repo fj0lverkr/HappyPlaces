8a9f2e50eb25692afe953a5f8e50fc42163b84b8
Nils Nahooy <nils.nahooy@live.com>
2022-09-25 23:05:36 +0200

    set up viewbinding

--- app/src/main/java/com/nilsnahooy/happyplaces/MainActivity.kt
+++ app/src/main/java/com/nilsnahooy/happyplaces/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nilsnahooy.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nilsnahooy.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var b: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b?.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        b = null
     }
 }
