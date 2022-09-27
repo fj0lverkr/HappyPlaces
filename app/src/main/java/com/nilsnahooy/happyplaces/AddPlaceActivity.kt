e8c0a443d2781d6716fb55d91306472eceff3f0c
Nils Nahooy <nils.nahooy@live.com>
2022-09-27 22:42:04 +0200

    set up basics for the add place activity

--- app/src/main/java/com/nilsnahooy/happyplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/nilsnahooy/happyplaces/AddPlaceActivity.kt
@@ -1,22 +1,16 @@
 package com.nilsnahooy.happyplaces
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nilsnahooy.happyplaces.databinding.ActivityAddPlaceBinding
-import com.nilsnahooy.happyplaces.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private var b: ActivityMainBinding? = null
+class AddPlaceActivity : AppCompatActivity() {
+    private var b: ActivityAddPlaceBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        b = ActivityMainBinding.inflate(layoutInflater)
+        b = ActivityAddPlaceBinding.inflate(layoutInflater)
         setContentView(b?.root)
-
-        b?.fabAddPlace?.setOnClickListener {
-            val intent = Intent(this, AddPlaceActivity::class.java)
-            startActivity(intent)
-        }
     }
 
     override fun onDestroy() {
