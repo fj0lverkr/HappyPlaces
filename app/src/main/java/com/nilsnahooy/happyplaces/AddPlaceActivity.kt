7a7244e2b2a94b5237d7ac1a8eec554f271d0f4f
Nils Nahooy <nils.nahooy@live.com>
2022-09-28 21:38:12 +0200

    properly set up actionbar

--- app/src/main/java/com/nilsnahooy/happyplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/nilsnahooy/happyplaces/AddPlaceActivity.kt
@@ -2,6 +2,7 @@ package com.nilsnahooy.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.nilsnahooy.happyplaces.databinding.ActivityAddPlaceBinding
 
 class AddPlaceActivity : AppCompatActivity() {
@@ -11,6 +12,19 @@ class AddPlaceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivityAddPlaceBinding.inflate(layoutInflater)
         setContentView(b?.root)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroy() {
