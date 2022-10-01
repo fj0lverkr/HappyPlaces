005475127d81a77d33b52f9d5533310a5b6cff3c
Nils Nahooy <nils.nahooy@live.com>
2022-10-01 11:49:44 +0200

    set up UI for add place activity.

--- app/src/main/java/com/nilsnahooy/happyplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/nilsnahooy/happyplaces/AddPlaceActivity.kt
@@ -3,6 +3,9 @@ package com.nilsnahooy.happyplaces
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.nilsnahooy.happyplaces.databinding.ActivityAddPlaceBinding
 
 class AddPlaceActivity : AppCompatActivity() {
@@ -15,6 +18,23 @@ class AddPlaceActivity : AppCompatActivity() {
 
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        b?.tilTitle?.markRequired()
+        b?.tilLocation?.markRequired()
+
+        b?.tilTitle?.editText?.setOnFocusChangeListener {
+                v, hasFocus ->
+            if (!hasFocus){
+                validateInput(v)
+            }
+        }
+
+        b?.tilLocation?.editText?.setOnFocusChangeListener {
+                v, hasFocus ->
+            if(!hasFocus){
+                validateInput(v)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -24,6 +44,20 @@ class AddPlaceActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    //extension function to show fields are required
+    private fun TextInputLayout.markRequired() {
+        hint = "$hint *"
+    }
+
+    private fun validateInput(v: View){
+        val view = v as TextInputEditText
+        if(view.text?.isEmpty() == true || view.text == null){
+            view.error = "required field!"
+        } else {
+            view.error = null
         }
     }
 
